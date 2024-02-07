a37e97889f1be69c9c6eb3c3b85a0a57080b5eb1
Jon Shipman <jon@jonshipman.com>
2024-02-06 20:00:36 -0600

    remove unused

--- src/lib/util/index.ts
+++ src/lib/util/index.ts
@@ -1,5 +1,1 @@
-export { default as InView } from './in-view.svelte';
-export { default as Resize } from './resize.svelte';
-export { default as Scroll } from './scroll.svelte';
-
 export { tailwind } from './theme';
--- src/lib/util/resize.svelte.d.ts
+++
@@ -1,9 +0,0 @@
-import type { SvelteComponent } from 'svelte';
-
-export interface ResizeDetail {
-	visible: { md: boolean };
-}
-
-export type ResizeEvent = Event & { detail: ResizeDetail };
-
-export default class Resize extends SvelteComponent {}

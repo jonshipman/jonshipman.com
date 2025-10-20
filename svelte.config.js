d1b3e35162947288cf5ac0ecc90114411590b683
Jon Shipman <jon@jonshipman.com>
2025-10-20 10:03:18 -0600

    chore: remove mdsvex requirement

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md'] })],
+	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter({
 			pages: 'build',
@@ -14,7 +13,7 @@ const config = {
 			strict: true
 		})
 	},
-	extensions: ['.svelte', '.svx', '.md']
+	extensions: ['.svelte']
 };
 
 export default config;
